3aa3997b4003fbf927c77d601f7dc41bd01ed517
BahaEddinDridi <bahaeddine170@gmail.com>
2024-06-22 23:04:09 +0100

    clean up

--- Backend/src/middlewares/authentication.js
+++ Backend/src/middlewares/authentication.js
@@ -1,19 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-    // Get token from header
     const token = req.header('x-auth-token');
 
-    // Check if not token
     if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
     }
 
     try {
-        // Verify token
         const decoded = jwt.verify(token,  process.env.JWT_SECRET);
-
-        // Add user from payload
         req.user = decoded;
         next();
     } catch (e) {
@@ -25,17 +20,14 @@ const checkPermissions = (requiredPermission) => {
     return (req, res, next) => {
         const { role, permissions } = req.user;
 
-        // Check if role is admin
         if (role === 'admin') {
             return next();
         }
 
-        // Check if permissions array exists and includes requiredPermission
         if (permissions && permissions.includes(requiredPermission)) {
             return next();
         }
 
-        // If not authorized
         return res.status(403).json({ message: 'Permission denied' });
     };
 };
